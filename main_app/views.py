6537106a7c83e818f4d09962656ec05a9dac359f
FrugalCarrot7 <james.devine1995@gmail.com>
2022-07-11 16:46:47 -0700

    adds index of recipes

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
-# Define the home view
+#temp data
+class Recipe:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, ingredients, description):
+    self.name = name
+    self.ingredients = ingredients
+    self.description = description
+
+recipes = [
+  Recipe('Pizza', 'Bread, Cheese, Tomato Sauce', 'tasty goodness'),
+  Recipe('Blueberry Pie', 'pie', 'A pie made with blueberries'),
+  Recipe('a food', 'its ingredients', 'its description')
+]
+
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def recipes_index(request):
+  return render(request, 'recipes/index.html', { 'recipes': recipes })
+
+  #update Recipe class above!!
