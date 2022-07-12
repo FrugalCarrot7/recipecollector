cd4ba97f32ca94acba0d4336cf242258db361f09
FrugalCarrot7 <james.devine1995@gmail.com>
2022-07-11 17:03:57 -0700

    adds recipe model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    ingredients = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Recipe
 
 #temp data
-class Recipe:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, ingredients, description):
-    self.name = name
-    self.ingredients = ingredients
-    self.description = description
 
-recipes = [
-  Recipe('Pizza', 'Bread, Cheese, Tomato Sauce', 'tasty goodness'),
-  Recipe('Blueberry Pie', 'pie', 'A pie made with blueberries'),
-  Recipe('a food', 'its ingredients', 'its description')
-]
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
     return render(request, 'about.html')
 
 def recipes_index(request):
-  return render(request, 'recipes/index.html', { 'recipes': recipes })
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/index.html', { 'recipes': recipes })
 
   #update Recipe class above!!
